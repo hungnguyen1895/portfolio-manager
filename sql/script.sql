00a818375463a5ba25497fb8135f1aa4ba240027
hungnguyen1895 <hn9581@gmail.com>
2021-08-13 19:44:45 +0000

    updated sql script

--- sql/script.sql
+++ sql/script.sql
@@ -10,13 +10,13 @@ CREATE TABLE `conygre`.`user` (
 
 CREATE TABLE `conygre`.`cash_account` (
                                           `id` INT NOT NULL,
-                                          `userId` INT NULL,
+                                          `user_id` INT NULL,
                                           `account_name` VARCHAR(45) NULL,
                                           `value` DOUBLE NULL,
                                           PRIMARY KEY (`id`),
-                                          INDEX `FK_cashaccount_user_id_idx` (`userId` ASC) VISIBLE,
+                                          INDEX `FK_cashaccount_user_id_idx` (`user_id` ASC) VISIBLE,
                                           CONSTRAINT `FK_cashaccount_user_id`
-                                              FOREIGN KEY (`userId`)
+                                              FOREIGN KEY (`user_id`)
                                                   REFERENCES `conygre`.`user` (`id`)
                                                   ON DELETE CASCADE
                                                   ON UPDATE CASCADE);
@@ -24,17 +24,18 @@ CREATE TABLE `conygre`.`cash_account` (
 
 CREATE TABLE `conygre`.`investment` (
                                         `id` INT NOT NULL,
-                                        `userId` INT NULL,
+                                        `user_id` INT NULL,
                                         `stock_name` VARCHAR(45) NULL,
                                         `quantity` DOUBLE NULL,
                                         `stock_symbol` VARCHAR(45) NULL,
                                         PRIMARY KEY (`id`),
-                                        INDEX `FK_investment_user_id_idx` (`userId` ASC) VISIBLE,
+                                        INDEX `FK_investment_user_id_idx` (`user_id` ASC) VISIBLE,
                                         CONSTRAINT `FK_investment_user_id`
-                                            FOREIGN KEY (`userId`)
+                                            FOREIGN KEY (`user_id`)
                                                 REFERENCES `conygre`.`user` (`id`)
                                                 ON DELETE NO ACTION
                                                 ON UPDATE NO ACTION);
+
 
 
 
@@ -44,12 +45,12 @@ INSERT INTO user (id, firstname, lastname, username, password) VALUES
 (2,'jessica', 'phelan', 'jessphelan', '123456'),
 (3, 'jovanny', 'vera', 'jovannyvera', '123456');
 
-INSERT INTO cash_account (id, userId, account_name, value) VALUES
+INSERT INTO cash_account (id, user_id, account_name, value) VALUES
 (1, 1, 'Citi', 3127.12),
 (2, 1, 'Well Fargo', 1223.12),
 (3, 1, 'Fidelity Cash', 6734.90);
 
-INSERT INTO investment (id, userId, stock_name, quantity, stock_symbol) VALUES
+INSERT INTO investment (id, user_id, stock_name, quantity, stock_symbol) VALUES
 (1, 1, 'TESLA', 31.25, 'TSLA'),
 (2, 1, 'Apple', 20, 'AAPL'),
 (3, 1, 'Citi', 200, 'C');
