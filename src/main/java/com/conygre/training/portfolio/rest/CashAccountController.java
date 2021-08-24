07065a3b0de714b568a1384d4f28e956c91f406d
hungnguyen1895 <hn9581@gmail.com>
2021-08-24 19:36:41 +0000

    Updated cross origin for cashaccount endpoints

--- src/main/java/com/conygre/training/portfolio/rest/CashAccountController.java
+++ src/main/java/com/conygre/training/portfolio/rest/CashAccountController.java
@@ -3,12 +3,10 @@ package com.conygre.training.portfolio.rest;
 import com.conygre.training.portfolio.service.CashAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/cashaccount")
 public class CashAccountController {
     @Autowired
